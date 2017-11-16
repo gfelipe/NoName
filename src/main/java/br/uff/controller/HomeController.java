20493eec5d62029a146545349971cce2425410d0
Felipe Gomes <felipe.deoliveira@b2wdigital.com>
2017-11-16 10:08:13 -0200

    Projeto

---
+++ src/main/java/br/uff/controller/HomeController.java
@@ -0,0 +1,25 @@
+package br.uff.controller;
+
+import br.uff.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+
+        model.addAttribute("alunos", alunoRepository.findAll());
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+
+}
---
+++ src/main/java/br/uff/repository/AlunoRepository.java
@@ -0,0 +1,7 @@
+package br.uff.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AlunoRepository extends PagingAndSortingRepository<Long, String> {
+
+}
