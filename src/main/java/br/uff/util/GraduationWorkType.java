6742e1076e5787af167bf82a8440a53964f6ea25
Felipe Ferreira <felipehfj@gmail.com>
2017-12-17 19:42:39 -0200

    Tipo de TCC

---
+++ src/main/java/br/uff/util/GraduationWorkType.java
@@ -0,0 +1,10 @@
+package br.uff.util;
+
+public enum GraduationWorkType {
+
+    TCC1("Trabalho de Conclusão de Curso 1", "TCC1"),
+    TCC2("Trabalho de Conclusão de Curso 1", "TCC2");
+
+    private String name;
+    private String abbreviation
+}
